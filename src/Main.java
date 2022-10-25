b9f022b42748a1bcd242a1383c837a76dc54ff75
JardimQueiroz <jardisonqueiroz@gmail.com>
2022-10-25 10:55:10 -0300

    final do projeto

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -18,6 +20,8 @@ public class Main {
         curso2.setDescricao("descricão curso JavaScript");
         curso2.setCargaHoraria(4);
 
+
+
         Mentoria mentoria = new Mentoria();
 
         mentoria.setTitulo("Mentoria Java");
@@ -25,9 +29,42 @@ public class Main {
         mentoria.setDate(LocalDate.now());
 
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        //System.out.println(curso1);
+        //System.out.println(curso2);
+        //System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Ifood Java Developer");
+        bootcamp.setDescicao("Descrição Bootcampo Ifood Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJardison = new Dev();
+        devJardison.setNome("Jardison");
+        devJardison.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devJardison.getConteudosInscritos());
+        devJardison.progredir();
+        devJardison.progredir();
+        devJardison.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos" + devJardison.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devJardison.getConteudosConcruidos());
+        System.out.println("XP: " + devJardison.calcularTotalXp());
+        System.out.println("-----------------------------");
+
+        Dev devMaria = new Dev();
+        devJardison.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devMaria.getConteudosInscritos());
+        devMaria.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Concluidos" + devMaria.getConteudosConcruidos());
+        System.out.println("Conteudos Inscritos" + devMaria.getConteudosInscritos());
+        System.out.println("XP: " + devMaria.calcularTotalXp());
+
+
 
 
     }
